0b39023cdd3bdfe70ca381eced6b022039e1ab0a
Soha Al Samaneh <soha.ms84@gmail.com>
2024-11-13 14:06:01 +0100

    First deploy

---
+++ api/app.py
@@ -0,0 +1,105 @@
+from fastapi import FastAPI, HTTPException
+import os
+from pydantic import BaseModel, ValidationError
+from predict import PreprocesseData
+from typing import Optional
+from enum import Enum
+
+# Set port to the env variable PORT to make it easy to choose the port on the server
+# If the Port env variable is not set, use port 8000
+PORT = os.environ.get("PORT", 8000)
+app = FastAPI(port=PORT)
+
+class PropertyType(str, Enum):
+    house = "HOUSE"
+    apartment = "APARTMENT"
+
+class BuildingState(str, Enum):
+    to_restore = "TO_RESTORE"
+    to_be_done_up = "TO_BE_DONE_UP"
+    to_renovate = "TO_RENOVATE"
+    just_renovated = "JUST_RENOVATED"
+    good = "GOOD"
+    as_new = "AS_NEW"
+
+
+class HeatingType(str, Enum):
+    carbon = "CARBON"
+    wood = "WOOD"
+    pellet = "PELLET"
+    fueloil = "FUELOIL"
+    gas = "GAS"
+    electric = "ELECTRIC"
+    solar = "SOLAR"
+
+class Province(str, Enum):    
+    brussels = "Brussels"
+    antwerp = "Antwerp"
+    eastFlanders = "EastFlanders"
+    flemish = "Flemish Brabant"
+    hainaut = "Hainaut"
+    limburg = "Limburg"
+    liege = "Liège"
+    luxembourg = "Luxembourg"
+    namur = "Namur"
+    walloon = "Walloon Brabant"
+    westFlander = "West Flanders"
+
+class EPC (str, Enum):
+    G = 'G'
+    F = 'F'
+    E = 'E'
+    D ='D'
+    C = 'C'
+    B = 'B'
+    A = 'A'
+    Aplus = 'A+'
+    ADpoblePluse =  'A++'
+
+class PropertyData(BaseModel):  
+
+    zip_code : Optional[int] = 0
+    total_area_sqm :  Optional[float] = 0
+    surface_land_sqm: float
+    nbr_frontages : Optional[int] = 0
+    nbr_bedrooms: int
+    terrace_sqm :  Optional[int] = 0
+    garden_sqm :  Optional[int] = 0
+    property_type : PropertyType
+    state_building :  BuildingState  
+    heating_type: HeatingType
+    province : Province
+    epc: EPC   
+   
+
+
+
+@app.get("/")
+async def root():
+    """Route that return 'Alive!' if the server runs."""
+    return {"Status": "Alive!"}
+
+
+@app.post("/predict")
+async def predict(propertyData : PropertyData):
+    """Route that do preprocessing data and predict property price"""
+    try:
+        new_data = propertyData.model_dump()
+        preprocessor = PreprocesseData(new_data)
+        prediction = preprocessor.preprocess_data()
+        print (prediction)
+        return {"prediction": float(prediction[0])}
+
+    except ValidationError as e:
+         # Custom error handling for validation errors
+        custom_errors = []
+        for error in e.errors():
+            field = "->".join(map(str, error['loc']))
+            custom_message = f"Error in field '{field}': {error['msg']}"
+            custom_errors.append({"field": field, "message": custom_message})
+        raise HTTPException(status_code=422, detail=custom_errors)
+    except TypeError as e:
+        print(f"TypeError: {e}")
+    except Exception as e:
+        # Handle unexpected errors
+        raise HTTPException(status_code=500, detail=str(e))
---
+++ api/predict.py
@@ -0,0 +1,91 @@
+import pandas as pd
+import joblib
+
+
+class  PreprocesseData:   
+
+    def __init__(self, new_data):
+        self.dataset = pd.DataFrame([new_data])
+  
+    def preprocess_data(self):
+        
+        input_df = self.dataset
+        new_data = preprocess_Categorical_data(input_df)
+      
+        # Path to the saved xgb_regressor model
+        # Load the trained xgb_regressor model 
+        model_path = 'xgb_model.joblib'   
+        model = load_model(model_path)
+
+        # Make predictions on the new data 
+        predictions = make_predictions(model, new_data)        
+        return  predictions
+
+
+def preprocess_Categorical_data(input_df):
+    #encode province column
+    # 'province_Antwerp'
+    all_provinces = ['province_Brussels', 'province_East Flanders',
+                    'province_Flemish Brabant', 'province_Hainaut', 
+                    'province_Limburg', 'province_Liège','province_Luxembourg', 
+                    'province_Namur', 'province_Walloon Brabant', 'province_West Flanders']
+    
+
+    # Initialize province columns to 0
+    for province in all_provinces:
+        input_df[province] = 0  
+
+    if 'province' in input_df.columns:
+        input_df['province'] = input_df['province'].str.lower()
+        for index, row in input_df.iterrows():
+            province_col = f"province{row['province']}"
+            if province_col in all_provinces:
+                input_df.at[index, province_col] = 1
+        input_df.drop('province', axis=1, inplace=True)
+
+    # Apply manual encoding 
+    # Mappings for 'state_building' , 'property_type' and 'epc' categories 
+    property_type_mapping = {
+        'HOUSE': 1, 'APARTMENT': 0}
+  
+    state_building_mapping = {
+        'TO_RESTORE': 0, 'TO_BE_DONE_UP': 1, 'TO_RENOVATE': 2,
+        'JUST_RENOVATED': 3, 'GOOD': 4, 'AS_NEW': 5
+    }
+
+    energy_mapping = {
+        'CARBON': 0, 'WOOD': 1,
+        'PELLET': 2, 'FUELOIL': 3,
+        'GAS': 4, 'ELECTRIC': 5,'SOLAR': 6,
+        }
+
+    epc_mapping = {
+            'G' : 0,'F': 1, 'E' : 2,'D' : 3,'C' : 4,
+            'B' : 5, 'A' : 6,'A+' : 7,'A++' : 8,      
+    }
+
+
+    if 'property_type' in input_df.columns:
+        input_df['property_type'] = input_df['property_type'].map(property_type_mapping).fillna(-1)
+  
+    if 'state_building' in input_df.columns:
+        input_df['state_building'] = input_df['state_building'].map(
+            state_building_mapping).fillna(-1)
+
+    if 'heating_type' in input_df.columns:
+        input_df['heating_type'] = input_df['heating_type'].map(
+            energy_mapping).fillna(-1)
+
+    if 'epc' in input_df.columns:
+        input_df['epc'] = input_df['epc'].map(epc_mapping).fillna(-1)
+
+    return input_df
+
+def load_model(model_path):
+    #Load the trained xgb_regressor model
+    return joblib.load(model_path)
+
+def make_predictions(model, new_data):
+    #Make predictions using the loaded model    
+    predictions = model.predict(new_data)   
+    return predictions
